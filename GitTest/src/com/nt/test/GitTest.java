bceee4be2fdea5e97d9a0bf1a4b8d434fe7ebb48
Subham Debadarshi Pani <49101016+subhampani467@users.noreply.github.com>
2019-05-28 19:02:36 +0530

    Update GitTest.java
    add a private method

--- GitTest/src/com/nt/test/GitTest.java
+++ GitTest/src/com/nt/test/GitTest.java
@@ -2,9 +2,11 @@ package com.nt.test;
 
 public class GitTest {
 
+	private String name;
+	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		system.out.println("HELLO");
+		System.out.println("HELLO");
 
 	}
 
